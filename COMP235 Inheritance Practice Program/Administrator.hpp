449bb11015e8d83701ef99ec06f955aaaf64ab99
gb_man <gb_man@gb-mans-iMac.local>
2017-04-04 16:39:11 -0700

    admin class header and imp file

---
+++ COMP235 Inheritance Practice Program/Administrator.hpp
@@ -0,0 +1,30 @@
+//
+//  Administrator.hpp
+//  COMP235 Inheritance Practice Program
+//
+//  Created by gb_man on 4/4/17.
+//  Copyright © 2017 alex. All rights reserved.
+//
+/**
+ Administrator Class
+ derived from Salaried Employee
+ change private to protected
+ supply additonal data and function members:
+ • member variable string contains administrators title
+ • member var string contains area of responsibility
+ • member var string contains supervisor name
+ • protencted member var double contains administrator annual salary
+ • member function setSupervisor changes supervisor name
+ • member function reads in admin's data from keyboard
+ • member function print outputs object data to screen
+ • overloading member printcheck with appropriate notations
+ 
+ 
+ **/
+
+#ifndef Administrator_hpp
+#define Administrator_hpp
+
+#include <stdio.h>
+
+#endif /* Administrator_hpp */
